ccddfcf2d537974944e7f522781f4bca42d7cbdf
Giorgia <giorgia.marenda@gmail.com>
2017-01-28 11:46:09 -0500

    Initial project configuration

---
+++ tomatoes-swift-sdk/TomatoesiOS/TomatoesiOS.h
@@ -0,0 +1,19 @@
+//
+//  TomatoesiOS.h
+//  TomatoesiOS
+//
+//  Created by Giorgia Marenda on 1/28/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TomatoesiOS.
+FOUNDATION_EXPORT double TomatoesiOSVersionNumber;
+
+//! Project version string for TomatoesiOS.
+FOUNDATION_EXPORT const unsigned char TomatoesiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TomatoesiOS/PublicHeader.h>
+
+
